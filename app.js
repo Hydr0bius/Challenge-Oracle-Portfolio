a31c944d05b386324c719a2dbd788fcd36f07133
Ricardo Polanco <polancoblancoricardo@gmail.com>
2023-07-12 19:55:06 -0600

    :art: add hamburger

--- app.js
+++ app.js
@@ -8,6 +8,22 @@ window.addEventListener("focus", () =>{
     document.title =docTitle;
 })
 
+//hamburger
+const bar = document.querySelector('#bar');
+const nav = document.querySelector('.header_nav');
+const cerrar = document.querySelector('#close');
+
+if(bar){
+    bar.addEventListener('click',()=>{
+        nav.classList.add('active');
+    });
+};
+if(cerrar){
+    cerrar.addEventListener('click',()=>{
+        nav.classList.remove('active');
+    });
+;}
+
 //efecto de transicion
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
