5d962ef8bb104b8def3f01fddb085a6a2b015636
Ricardo Polanco <polancoblancoricardo@gmail.com>
2023-07-22 14:21:39 -0600

    :building_construction: :children_crossing: V2.0

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 //efecto para el titulo
 let docTitle = document.title;
 window.addEventListener("blur",() =>{
@@ -24,10 +23,57 @@ if(cerrar){
     });
 ;}
 
+//efecto type
+function animacion(){
+    let textoAnimacion =[
+        ['c','o','n','s','t','r','u','y','o',' ','p','á','g','i','n','a','s',' ','w','e','b'],
+        ['s','o','y',' ','p','r','o','g','r','a','m','a','d','o','r'],
+    ];
+    let letraContador = -1;
+    let nivelArray = 0;
+
+    const contenedorAnimaicon = document.querySelector('.cont_text_animation');
+
+    function pintarTexto(){
+        letraContador++;
+        contenedorAnimaicon.textContent += textoAnimacion[nivelArray][letraContador];
+
+        if(letraContador === textoAnimacion[nivelArray].length -1){
+            clearInterval(intervalo);
+
+            setTimeout(()=>{
+                intervalo = setInterval(()=>{
+                    contenedorAnimaicon.textContent = '';
+                    letraContador--;
+                    textoAnimacion[nivelArray].pop();
+
+                    textoAnimacion[nivelArray].forEach((elemento)=>{
+                        contenedorAnimaicon.textContent += elemento;
+                    });
+
+                    if(letraContador < 0){
+                        clearInterval(intervalo);
+                        nivelArray++;
+                        intervalo = setInterval(pintarTexto,150);
+
+                        if(nivelArray > textoAnimacion.length -1){
+                            clearInterval(intervalo);
+                            nivelArray = 0;
+                            animacion();
+                        };
+                    };
+                },150);
+            },1000);
+        };
+    };
+    let intervalo = setInterval(pintarTexto,150);
+};
+window.addEventListener("load",animacion);
+
 //efecto de transicion
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
+        // console.log(entry)
         if (entry.isIntersecting){
             entry.target.classList.add('show');
         } else{
